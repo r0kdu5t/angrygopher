775b24d4e195328e84442082763e3bef1211ef9b
Andrew Sands <r0kdu5t@theatrix.org.nz>
2018-11-17 18:22:57 +1300

    Added some files for reference.

---
+++ help.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+#
+#  For reference - See: https://www.linuxjournal.com/article/3946
+#
+
+import sys
+if '-h' in sys.argv or '-help' in sys.argv or '--help' in sys.argv:
+    print '''
+help.py--does nothing useful (yet)
+options: -h, -help, or --help -> display this help
+Copyright (c) Jacek Artymiak, 2000 '''
+    sys.exit(0)
+else:
+    print("I don't recognize this option!")
+    sys.exit(0)
